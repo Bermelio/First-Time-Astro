53856c42a7fd6adb09079a18ec2f13641db7ef62
Bermelio <facundoperalta258@gmail.com>
2026-01-04 04:48:06 -0300

    testing metadata open graph

---
+++ src/consts/site-info.ts
@@ -0,0 +1,5 @@
+export const siteInfo = {
+  title: 'Mi propia pokedex',
+  description: 'Esta pagina esta hecha para practicas no profesionales',
+  image: 'https://pbs.twimg.com/profile_images/1952706057882476545/7UcNf6gN.jpg'
+}
